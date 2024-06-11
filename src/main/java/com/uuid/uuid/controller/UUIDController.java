be3728956706f1d27b6588a8abc61551fb7a8241
sidhant97 <guptasidhant1997@gmail.com>
2024-06-11 08:28:55 +0530

    Initial project setup

--- src/main/java/com/uuid/uuid/controller/UUIDController.java
+++ src/main/java/com/uuid/uuid/controller/UUIDController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("${api.uuidcontext}")
 public class UUIDController {
 
-
     @Autowired
     private UUIDService uuidService;
 
